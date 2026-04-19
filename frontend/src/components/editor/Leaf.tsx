c7a5722c080bec9a61658fcf32b604fd0fb8195e
igor <yu@igordeMacBook-Pro.local>
2026-04-19 19:28:47 +0800

    add: frontend/src/components/editor/Leaf.tsx

---
+++ frontend/src/components/editor/Leaf.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import type { CustomText } from './custom-types'
+
+interface LeafProps {
+  attributes: any
+  children: React.ReactNode
+  leaf: CustomText
+}
+
+export const Leaf: React.FC<LeafProps> = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>
+  }
+
+  if (leaf.strikethrough) {
+    children = <del>{children}</del>
+  }
+
+  return <span {...attributes}>{children}</span>
+}
